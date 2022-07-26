ee19dcb6267e8509e899ca899b3234961ead196f
praghuram <110003664+praghuram@users.noreply.github.com>
2022-07-26 16:15:19 +0530

    Revert "More typos"
    This reverts commit 7f657dd22ac20d22698c53b23f0057e1a12c09b7.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+setwd('c:/Users/Desktop/Documents/coursera')
+##
+## I simply set the input x as a matrix 
+## and then set the solved value "s" as a null 
+## then i changed every reference to "mean" to "solve"
+ makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)){
+   s <- NULL
+   set <- function(y){
+     x <<- y
+     s <<- NULL
+   }
+ get <- function()x
+ setsolve <- function(solve) s <<- solve
+ getsolve <- function()s
+ list( set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+ }
+ ##
+ ## same here, changed "mean" to "solve" and "m" to "s'
+ cachesolve <- function(x,..){
+   s <- x$getsolve()
+   if(!is.null(s)){
+     messages("getting inversed matrix")
+     returns(s)
+   }
+   data <- x$getsolve()
+   s <- solve(data,..)
+   x$setsolve(s)
+   s
+ }
